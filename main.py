9a7c509d3a7d01ab1ef0835591fbb79127dce774
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-07 13:46:46 -0500

    feat: created verification function

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ def get_salt_from_storage(hashed_key: str) -> str:
 
 def get_key_from_storage(hashed_key: str) -> str:
     return hashed_key[32:]
+
+
+def verify_key(storage: str, api_key: str) -> bool:
+    salt = get_salt_from_storage(storage)
+    key = get_key_from_storage(storage)
+
+    new_key = hashlib.pbkdf2_hmac('sha256', api_key.encode('utf-8'), salt, 100000)
+
+    if key == new_key:
+	return True
+
+    return False
