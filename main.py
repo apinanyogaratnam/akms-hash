00cec64dd2538a92a56293decfc9f66fbc1c81b4
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-07 14:43:43 -0500

    style: formatting code

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import os
 import hashlib
+import os
 
 
 def hash_api_key(api_key: str) -> str:
     salt = os.urandom(32)
-    key = hashlib.pbkdf2_hmac('sha256', api_key.encode('utf-8'), salt, 100000)
+    key = hashlib.pbkdf2_hmac("sha256", api_key.encode("utf-8"), salt, 100000)
 
     storage = salt + key
 
@@ -23,6 +23,6 @@ def verify_key(storage: str, api_key: str) -> bool:
     salt = get_salt_from_storage(storage)
     hashed_api_key = get_hashed_api_key_from_storage(storage)
 
-    received_hashed_api_key = hashlib.pbkdf2_hmac('sha256', api_key.encode('utf-8'), salt, 100000)
+    received_hashed_api_key = hashlib.pbkdf2_hmac("sha256", api_key.encode("utf-8"), salt, 100000)
 
     return hashed_api_key == received_hashed_api_key
