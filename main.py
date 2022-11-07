28b18912196ea10ef77f2edd3fd325cb3eb3885e
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-07 14:38:56 -0500

    fix: typo of variable

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def get_hashed_api_key_from_storage(storage: str) -> str:
 
 def verify_key(storage: str, api_key: str) -> bool:
     salt = get_salt_from_storage(storage)
-    hased_api_key = get_hashed_api_key_from_storage(storage)
+    hashed_api_key = get_hashed_api_key_from_storage(storage)
 
     received_hashed_api_key = hashlib.pbkdf2_hmac('sha256', api_key.encode('utf-8'), salt, 100000)
 
