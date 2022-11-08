f776e3ed691cb2841936fbc5c6d117d4750674b6
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-08 08:34:53 -0500

    refactor: moved all code into akms_hash dir

--- akms_hash/main.py
+++ akms_hash/main.py
@@ -11,17 +11,17 @@ def hash_api_key(api_key: str) -> bytes:
     return storage
 
 
-def get_salt_from_storage(storage: bytes) -> bytes:
+def __get_salt_from_storage(storage: bytes) -> bytes:
     return storage[:32]
 
 
-def get_hashed_api_key_from_storage(storage: bytes) -> bytes:
+def __get_hashed_api_key_from_storage(storage: bytes) -> bytes:
     return storage[32:]
 
 
-def verify_key(storage: str, api_key: str) -> bool:
-    salt = get_salt_from_storage(storage)
-    hashed_api_key = get_hashed_api_key_from_storage(storage)
+def verify_api_key(storage: str, api_key: str) -> bool:
+    salt = __get_salt_from_storage(storage)
+    hashed_api_key = __get_hashed_api_key_from_storage(storage)
 
     received_hashed_api_key = hashlib.pbkdf2_hmac("sha256", api_key.encode("utf-8"), salt, 100000)
 
