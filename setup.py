781a9e2d2c587b098b8ecad3b7622eef47ae2ba9
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-12 15:45:04 -0500

    chore: updated version to 0.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="akms-hash",
-    version="0.2.1",
+    version="0.2.2",
     author="apinanyogaratnam",
     author_email="apinanapinan@icloud.com",
     description="An API Key hashing library",
