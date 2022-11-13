bf763b1a8fd3f4ae464e464ae659009586b2a103
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-12 20:56:19 -0500

    feat: refactored to using hex encrypted data

--- akms_hash/main.py
+++ akms_hash/main.py
@@ -1,32 +1,23 @@
 import hashlib
-import os
 
 
-def __hash_api_key(api_key: str, salt: bytes) -> bytes:
-    return hashlib.pbkdf2_hmac("sha256", api_key.encode("utf-8"), salt, 100000)
+def __hash_api_key(api_key: bytes, salt: bytes) -> bytes:
+    return hashlib.pbkdf2_hmac("sha256", api_key, salt, 100000)
 
 
-def hash_api_key(api_key: str) -> bytes:
-    salt = os.urandom(32)
-    key = __hash_api_key(api_key, salt)
+def hash_api_key(api_key: str, salt: str) -> str:
+    salt = salt.encode("utf-8")
+    api_key = api_key.encode("utf-8")
 
-    storage = salt + key
+    hashed_api_key = __hash_api_key(api_key, salt)
 
-    return storage
-
-
-def __get_salt_from_storage(storage: bytes) -> bytes:
-    return storage[:32]
+    return hashed_api_key.hex()
 
 
-def __get_hashed_api_key_from_storage(storage: bytes) -> bytes:
-    return storage[32:]
+def verify_api_key(received_api_key: str, hashed_api_key: str, salt: str) -> bool:
+    received_api_key = received_api_key.encode("utf-8")
+    salt = salt.encode("utf-8")
 
-
-def verify_api_key(storage: bytes, api_key: str) -> bool:
-    salt = __get_salt_from_storage(storage)
-    hashed_api_key = __get_hashed_api_key_from_storage(storage)
-
-    received_hashed_api_key = __hash_api_key(api_key, salt)
+    received_hashed_api_key = __hash_api_key(received_api_key, salt).hex()
 
     return hashed_api_key == received_hashed_api_key
