8f37f5b0b6217a21e56a85798036f68e7b348882
apinanyogaratnam <apinanapinan@icloud.com>
2022-11-12 20:56:35 -0500

    test: updated test cases for hex hashing

--- tests/unittests/test_main.py
+++ tests/unittests/test_main.py
@@ -3,18 +3,23 @@ from akms_hash.main import hash_api_key, verify_api_key
 
 def test_hash_api_key():
     api_key = 'test'
-    hashed_api_key = hash_api_key(api_key)
-    assert isinstance(hashed_api_key, bytes)
+    salt = 'test'
+    hashed_api_key = hash_api_key(api_key, salt)
+    assert isinstance(hashed_api_key, str)
+    assert len(hashed_api_key) == 64
 
 
 def test_verify_api_key():
     api_key = 'test'
-    hashed_api_key = hash_api_key(api_key)
-
+    salt = 'test'
+    hashed_api_key = hash_api_key(api_key, salt)
+    print('hashed_api_key: ', hashed_api_key)
     expected = True
-    actual = verify_api_key(hashed_api_key, api_key)
+    actual = verify_api_key(api_key, hashed_api_key, salt)
+    print('actual: ', actual)
     assert actual == expected
 
+    api_key = 'test1'
     expected = False
-    actual = verify_api_key(hashed_api_key, 'test1')
+    actual = verify_api_key(api_key, hashed_api_key, salt)
     assert actual == expected
